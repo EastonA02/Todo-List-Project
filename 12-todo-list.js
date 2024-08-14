a741df5d92dcf0347c1a41ca051230e7c0a4489d
EastonA02 <136672562+EastonA02@users.noreply.github.com>
2024-08-14 14:11:23 -0700

    Add files via upload

---
+++ 12-todo-list.js
@@ -0,0 +1,71 @@
+const todoList = [{
+  name: 'make dinner', 
+  dueDate: '2-22-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+
+let todoListHTML = ' ';
+
+
+  todoList.forEach((todoObject, index) => {
+    const {name, dueDate} = todoObject;
+   
+    const html = `
+    <div> ${name}</div>
+    <div>${dueDate}</div> 
+        
+    <button class="delete-todo-button js-delete-todo-button">
+      Delete
+    </button> 
+    `;
+        
+    todoListHTML += html;
+  })
+
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML;
+
+  //html is generated onto page, now we can add event listener for delete button
+  document.querySelectorAll('.js-delete-todo-button')
+    .forEach((deleteButton, index) => { //loop though items to delete
+      //listens for which button was clicked to be deleted
+      deleteButton.addEventListener('click', () => { 
+        todoList.splice(index, 1);
+        renderTodoList();
+      });
+    })
+
+}
+
+document.querySelector('.js-add-todo-button').
+addEventListener('click', () => {
+  addTodo();
+})
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+  
+  todoList.push({
+  //name: name,
+  //dueDate: dueDate
+  name, 
+  dueDate
+});
+
+
+  inputElement.value = ' ';
+
+  renderTodoList();
+
+}
